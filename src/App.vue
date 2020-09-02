8a807a3e435d4627446c93826a973c33c091d2da
Adrian <revanskyw@gmail.com>
2020-09-01 20:46:15 -0400

    se añade css

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" >
+      <div v-for="(p, i) in personajes" :key="i">
+        <Personaje  :src="p.imagen" :name="p.nombre"  />
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Personaje from "./components/personaje";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Personaje,
+  },
+  data() {
+    return {
+      personajes: [],
+      
+    };
+  },
+  methods: {
+   
+  },
+  mounted() {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => response.json())
+      .then((json) => {
+        let data = json.results;
+     
+        data.forEach((el) => {
+          let nombre = el.name;
+          let imagen = el.image;
+
+          this.personajes.push({
+            nombre,
+            imagen,
+          });
+        });
+      })
+      .catch((err) => {
+        console.log(`error en  ${err}`);
+      });
+  },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
+  display: flex;
+  flex-wrap: wrap;
+  margin: auto;
+  justify-content: space-around;
+  margin: 10px;
+}
+
+
+.img{
+  height:300px;
+  background-image: url("../src/assets/ricklogo.png");
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+body{
+
+
+  background: green;
+}
+
+h3{
+
+color: white;
+text-align: center;
+font-weight: bold;
+
 }
 </style>
---
+++ src/components/personaje.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <img :src="src" alt />
+  <h3 >{{name}}</h3>
+</div>
+</template>
+
+
+
+
+
+<script>
+export default {
+  name: "Personaje",
+  props: {
+    src: String,
+    name: String,
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
